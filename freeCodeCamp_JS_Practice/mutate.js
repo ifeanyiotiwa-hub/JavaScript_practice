028aed91a05133770dbd298e2153a44d1d122f52
ifeanyiotiwa-hub <ifeanyichukwuotiwa.winson@gmail.com>
2021-08-04 15:09:12 +0100

    Addded comment to mutate.js

--- freeCodeCamp_JS_Practice/mutate.js
+++ freeCodeCamp_JS_Practice/mutate.js
@@ -1,13 +1,16 @@
 function mutation(arr) {
-    let str0 = arr[0].toLowerCase();
-    let str1 = arr[1].toLowerCase();
-    let tempArr = [str0, str1];
+    let str0 = arr[0].toLowerCase();// convert first element of arr to Lowercase
+    let str1 = arr[1].toLowerCase(); // convert second element of arr to Lowercase
+    let tempArr = [str0, str1]; // create a tempArr containing arr elements in lowerCase
+    // loop through temp arr
     for (let i = 0; i < tempArr[1].length; i++){
+        //check if tempArr element at index 1 is not included in tempArr[0] and return false
+        //else continue to the next iteration
       if (!tempArr[0].includes(tempArr[1][i])){
           return false;
-      }
+      } // end if
     }
-    
+    // return true since it passes the if test
     return true;
   }
   
